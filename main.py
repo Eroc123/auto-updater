dd9c8e839aa36c01c182f538a09f7509ab7962e1
Eroc123 <79434990+Eroc123@users.noreply.github.com>
2022-07-01 22:21:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import updater
+
+VERSION = '0.1'
+
+def update(file):
+    pass
+
+file, isoutdated = updater.get_update('https://raw.githubusercontent.com/Eroc123/auto-updater/release/main.py', VERSION)
+if isoutdated:
+    update(file)
